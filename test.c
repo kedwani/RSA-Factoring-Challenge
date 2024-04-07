81855a0ed972d1982bd20fcc1dd0ad930d026ece
kedwani linux <mahmoudnasser11566@gmail.com>
2024-04-07 02:08:17 +0200

    auto push

--- test.c
+++ test.c
@@ -3,8 +3,14 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
-
-int main(int argc, char** argv)
+/**
+ *main - function to find the factor of numbers stored in a file
+ *line by line.
+ *@argc : is the number of arguments
+ *@argv : is the passed arguments
+ *Return: 0 if success
+ */
+int main(int argc, char **argv)
 {
 	int o;
 	ssize_t read_bytes;
@@ -14,18 +20,18 @@ int main(int argc, char** argv)
 	if (argc != 2)
 	{
 		printf("Usage: %s <filename>\n", argv[0]);
-		return 1;
+		return (1);
 	}
 	o = open(argv[1], O_RDONLY);
-	if (o == -1) {
+	if (o == -1)
+	{
 		perror("Error opening file");
-		return 1;
+		return (1);
 	}
 	while ((read_bytes = read(o, buff, sizeof(buff) - 1)) > 0)
 	{
 		buff[read_bytes] = '\0';
 		ptr = buff;
-
 		while (*ptr)
 		{
 			n = strtoull(ptr, &ptr, 10);
@@ -34,19 +40,21 @@ int main(int argc, char** argv)
 			for (calc = 2; calc <= n; ++calc)
 				if ((n % calc) == 0)
 				{
-					printf("%llu=%llu*%llu\n", n, calc, n / calc); // Change format specifiers to %llu
+					printf("%llu=%llu*%llu\n", n, calc, n / calc);
 					break;
 				}
 		}
 	}
-	if (read_bytes == -1) {
+	if (read_bytes == -1)
+	{
 		perror("Error reading file");
 		close(o);
-		return 1;
+		return (1);
 	}
-	if (close(o) == -1) {
+	if (close(o) == -1)
+	{
 		perror("Error closing file");
-		return 1;
+		return (1);
 	}
-	return 0;
+	return (0);
 }
